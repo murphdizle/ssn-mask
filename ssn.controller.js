6ee8c89e6ab6c8fcd5acb73bf9aa47dd00d3d5e7
Amanda Murphy <murphdizle@Hotmail.com>
2016-12-23 23:32:01 -0800

    Update ssn.controller.js

--- ssn.controller.js
+++ ssn.controller.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2016 Amanda Murphy 
+// This code is available under the "MIT License".
+// Please see the file COPYING in this distribution
+// for license terms.
 
 class SsnController {
     constructor($scope) {
